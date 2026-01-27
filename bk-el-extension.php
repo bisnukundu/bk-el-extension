5d6d8dbef02a9d722c82451f367f74c92e3eef41
bisnukundu <bisnukundu@gmail.com>
2026-01-27 19:32:54 +0600

    init

---
+++ bk-el-extension.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: BK Elementor Extension
+ * Version: 1.0.0
+ * Description: This is a Elementor Widgets extension.
+ * Author: Bisnu Kundu
+ * Author URI: https://bisnukundu.netlify.app/
+ * Requires Plugins: elementor
+ * 
+ */
+
+Class Bk_el_extension{
+    function __construct(){
+
+    }
+    
+}
+
+new Bk_el_extension();
